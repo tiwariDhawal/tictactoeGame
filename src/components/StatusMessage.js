635757d87edea610579ceb7c3adca99e88d9e8b5
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-06-15 13:20:34 +0530

    styled the entire app

--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -3,12 +3,29 @@ import React from "react";
 export const StatusMessage = ({ winner, current }) => {
   const noMoveLeft = current.board.every((element) => element !== null);
   return (
-    <h2>
-      {winner && `Winner is ${winner}`}
-      {!winner &&
-        !noMoveLeft &&
-        `Next player is ${current.isXNext ? "X" : "O"}`}
-      {!winner && noMoveLeft && "X and O tied"}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          Winner is{" "}
+          <span className={winner === "X" ? "text-green" : "text-orange"}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMoveLeft && (
+        <>
+          Next player is{" "}
+          <span className={current.isXNext ? "text-green" : "text-orange"}>
+            {current.isXNext ? "X" : "0"}{" "}
+          </span>
+        </>
+      )}
+      {!winner && noMoveLeft && (
+        <>
+          <span className="text-green">X</span>{" "}and{" "}
+          <span className="text-orange">0</span>{" "}tied
+        </>
+      )}
+    </div>
   );
 };
